0a11d193f0520d09497c4917e53c0b4b2855daa0
Nshuti Maurice <nshutikope@gmail.com>
2025-12-14 16:02:29 +0200

    chore: add comment to cart routes

--- routes/cart.js
+++ routes/cart.js
@@ -1,15 +1,19 @@
 /** @format */
 
 import express from "express";
+import { requireAuth } from "../middleware/requireAuth.js";
 import {
   addToCart,
   getCartCount,
   getAll,
   deleteItem,
+  deleteAll,
 } from "../controllers/cartController.js";
 
+// Cart routes
 export const cartRouter = express.Router();
-cartRouter.post("/add", addToCart);
-cartRouter.get("/cart-count", getCartCount);
-cartRouter.get("/", getAll);
-cartRouter.delete("/:itemId", deleteItem);
+cartRouter.post("/add", requireAuth, addToCart);
+cartRouter.get("/cart-count", requireAuth, getCartCount);
+cartRouter.get("/", requireAuth, getAll);
+cartRouter.delete("/all", requireAuth, deleteAll);
+cartRouter.delete("/:itemId", requireAuth, deleteItem);
